42203e3f987de3341cb2aa167250c493cd007703
Pascal Stucheli <pascastu@student.ethz.ch>
2012-03-27 20:23:48 +0200

    27.3.12 Ps the script is with latent phase and true stochastic

--- code/SEI_stochastic.m
+++ code/SEI_stochastic.m
@@ -2,15 +2,15 @@
 clear all
 
 hold on
-for i = 1:100
+for i = 1:2
     i
     
     %parameter definition
     N = 10000; %population
-    dt = 0.25; %hours
-    beta = 0.05; %contact rate
-    eps = 1/24; %average latent period = 1/eps
-    endtime = 24*7; %hours
+    dt = 60; %mins
+    beta = 0.002; %contact rate
+    eps = 1/(24*60); %average latent period = 1/eps
+    endtime = 24*60*2; %minutes
     
     %initialization
     j = 1;
@@ -23,7 +23,7 @@ for i = 1:100
         sig_dEdt1 = sqrt(dEdt1);
         dEdt2 = E(j)*eps;
         if dEdt2 > E(j)
-            dEdt2 = E(j)
+            dEdt2 = E(j);
         end
         sig_dEdt2 = sqrt(dEdt2);
         
@@ -31,11 +31,11 @@ for i = 1:100
         dEdt2f = -1;
         while dEdt1f < 0 %random number has to be 0 or bigger
             rn = randn; % standard normal random number
-            dEdt1f = round((dEdt1*dt+sig_dEdt1*rn));
+            dEdt1f = round((dEdt1*dt+sig_dEdt1*rn*sqrt(dt)));
         end
         while dEdt2f < 0
             rn = randn;
-            dEdt2f = round((dEdt2*dt+sig_dEdt2*rn));
+            dEdt2f = round((dEdt2*dt+sig_dEdt2*rn*sqrt(dt)));
         end
         
         E(j+1) = E(j)+dEdt1f-dEdt2f; % I_new = I_old + E(dI)*dt + std(dI)*sqrt(dt)*randn
@@ -44,7 +44,7 @@ for i = 1:100
         j=j+1; % uptdate counter
     end
     
-    plot(t,I)
+    plot(t,I,'b')
     plot(t,E,'r')
 end
 hold off
