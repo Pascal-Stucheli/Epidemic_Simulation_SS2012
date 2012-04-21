08089cd93f9ac4072363439cda4813d34bb965e1
jordich <jordich@student.ethz.ch>
2012-04-21 14:49:47 +0200

    12.4.21.jI.Networkgeneration_added

---
+++ code/Networkgeneration/networkepid.m
@@ -0,0 +1,46 @@
+%20.4.2012 Christian Jordi
+%Generation of a scale free network
+
+clear all  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%to be removed
+
+%Parameters
+ncit= 2000; %number of cities
+
+%variables
+seeded = [1 5; 2 3; 4 5;4 3];%seed for the edges
+seedct = [1 0 0;1 0 0;1 0 0;1 0 0;2 0 0]; %seed for the cities
+cities=zeros(ncit,3);
+pos= length(seedct(:,1));
+cities(1:pos,1:3)=seedct; %Stores (degree,Susceptibles,Infected)
+edge=seeded; %Connections btwn the cities, store lower index first
+pass=0; %number of passengers on an edge
+mlinks=1; %number of links that are fixed at each step
+sumlinks=length(edge(:,1)); %total number of edges
+linkage=0; %Determines wether a new edge has been set 
+
+%functions
+while pos < ncit
+    pos= pos+1;
+    linkage=0;
+    
+    while linkage ~= mlinks %true as long as the new edge has not been set
+        rnode = ceil(rand * pos); %randomly choose a node in front of the current position
+        deg = cities(rnode,1); %degree of the chosen node
+        rlink = rand; %random number to determine wether an edge is formed
+        
+         if rlink < deg / sumlinks %&& Net(pos,rnode) ~= 1 && Net(rnode,pos) ~= 1 as long as only one linkage is set per step
+                                   % (mlinks=1) the oher two conditions do not matter
+            cities(pos,1)=1; %degree of the new introduced city is set to 1
+            cities(rnode,1)=cities(rnode,1)+1; %The deg of the chosen city is increased by one
+            edge(pos,1)=rnode; %stores the new edge
+            edge(pos,2)=pos;
+            sumlinks=length(edge(:,1));
+            linkage=linkage+1;
+         end
+    end
+end
+
+edgecell=num2cell(edge);
+cell2csv('network.csv', edgecell, [], 2007, [])
+cities
+edge
