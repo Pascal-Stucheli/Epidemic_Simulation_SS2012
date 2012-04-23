37f40528e9297767ab307b83a830e7aa74abc920
jordich <jordich@student.ethz.ch>
2012-04-23 14:01:30 +0200

    12.4.23.jIchanges_in_Network

--- code/Networkgeneration/networkepid.m
+++ code/Networkgeneration/networkepid.m
@@ -1,10 +1,10 @@
 %20.4.2012 Christian Jordi
 %Generation of a scale free network
 
-clear all  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%to be removed
+clear all  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%to be removed ->insert function definition here
 
 %Parameters
-ncit= 20000; %number of cities
+ncit= 1000; %number of cities
 
 %variables
 seeded = [1 5; 2 3; 4 5;4 3];%seed for the edges
@@ -13,7 +13,7 @@ cities=zeros(ncit,3);
 pos= length(seedct(:,1));
 cities(1:pos,1:3)=seedct; %Stores (degree,Susceptibles,Infected)
 edge=seeded; %Connections btwn the cities, store lower index first
-pass=0; %number of passengers on an edge
+pass=0; %number of passengers on an edge %%%%%%not used yet%%%%%%%%%
 mlinks=1; %number of links that are fixed at each step
 sumlinks=length(edge(:,1)); %total number of edges
 linkage=0; %Determines wether a new edge has been set 
@@ -42,6 +42,8 @@ while pos < ncit
 end
 %bring data in a form gephi can understand
 edgecell=num2cell(edge);
+edgecell2{length(edgecell),3}=0;
+
 
 for i=1:length(edgecell)
     edgecell2{i+1,1}=edgecell{i,1};
@@ -53,4 +55,4 @@ edgecell2{1,1}='source';
 edgecell2{1,2}='target';
 edgecell2{1,3}='type';
 
-cell2csv('network.csv', edgecell2, [], 2007, [])
+cell2csv('network2.csv', edgecell2, [], 2007, [])
