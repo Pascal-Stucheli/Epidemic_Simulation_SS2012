8d66b649f5618ebc6b9cb913694041c1583096f8
jordich <jordich@student.ethz.ch>
2012-05-01 11:44:18 +0200

    some comment added

--- code/Initialisation/Initialisation.m
+++ code/Initialisation/Initialisation.m
@@ -2,6 +2,10 @@
 %everything needed is stored in .txt or .csv files. -> has only to be
 %executed once
 
+%This function generates:   -cities.txt w/o infection
+%                           -edges.txt
+%                           -network.csv
+%                           -tot_T.txt
 
 function[]=Initialisation()
 
@@ -12,6 +16,7 @@ dt=2; %2hours
 
 %[cities,edge]=networkepid(ncit,seeded,seedct); %->skipped the saving step
 [cities,edge]=networkepid2loops(ncit,seeded,seedct); %generates a network with loops instead of dead ends
+
 cities=city_size(cities); %output is cities.txt
 
 tot_T = generate_fixed_tot_T(cities,edge,dt);
