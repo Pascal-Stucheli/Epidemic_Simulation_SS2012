15f12f082387e9f993889c44bf5b142ecbb61859
jordich <jordich@student.ethz.ch>
2012-05-01 11:18:23 +0200

    Network with loops addedII

--- code/Initialisation/Initialisation.m
+++ code/Initialisation/Initialisation.m
@@ -10,8 +10,8 @@ seeded = [1 5; 2 3; 4 5;4 3];%seed for the edges
 seedct = [1 0 0;1 0 0;1 0 0;1 0 0;2 0 0]; %seed for the cities
 dt=2; %2hours
 
-[cities,edge]=networkepid(ncit,seeded,seedct); %->skipped the saving step
-
+%[cities,edge]=networkepid(ncit,seeded,seedct); %->skipped the saving step
+[cities,edge]=networkepid2loops(ncit,seeded,seedct); %generates a network with loops instead of dead ends
 cities=city_size(cities); %output is cities.txt
 
 tot_T = generate_fixed_tot_T(cities,edge,dt);
