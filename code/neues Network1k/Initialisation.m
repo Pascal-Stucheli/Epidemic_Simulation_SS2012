e337bf888004fe501fb03a92bc61b89fde356692
jordich <jordich@student.ethz.ch>
2012-05-21 15:44:56 +0200

    todays changes

---
+++ code/neues Network1k/Initialisation.m
@@ -0,0 +1,28 @@
+%Summary of the functions needed to create our start network. bcs
+%everything needed is stored in .txt or .csv files. -> has only to be
+%executed once
+
+%This function generates:   -cities.txt w/o infection
+%                           -edges.txt
+%                           -network.csv
+%                           -tot_T.txt
+
+function[]=Initialisation()
+
+ncit = 500; %number of cities in the Network
+seeded = [1 5; 2 3; 4 5;4 3];%seed for the edges
+seedct = [1 0 0;1 0 0;1 0 0;1 0 0;2 0 0]; %seed for the cities
+dt=2; %2hours
+
+%[cities,edge]=networkepid(ncit,seeded,seedct); %->skipped the saving step
+[cities,edge]=networkepid2loops(ncit,seeded,seedct); %generates a network with loops instead of dead ends
+
+cities=city_size(cities); %output is cities.txt
+
+tot_T = generate_fixed_tot_T(cities,edge,dt);
+
+dlmwrite('tot_T.txt',tot_T);
+
+%%%%%Add infection...
+
+end
---
+++ code/neues Network1k/generate_fixed_tot_T.m
@@ -0,0 +1,43 @@
+function tot_T = generate_fixed_tot_T(cities,edges,dt)
+% This function is meant to be the interface between the network and the disease simulation.
+
+k = cities(:,1); % degree
+
+N = cities(:,2); % Total population
+I = cities(:,3); % Infected
+S = N - I;	 % Total p
+
+k_mean = mean(k);
+
+% Timefactor
+tf = dt/24;
+
+% Transport matrix
+T = zeros(length(edges),2); % col1: Infected voyagers going x -> y; col2: Infected voyagers going y -> x)
+
+for	i = 1:length(edges)
+    
+    x = edges(i,1); % It is not necessary to define x and y but it may helps with the overview
+    y = edges(i,2);
+    
+    % We weight the transport along an edge with the degrees of the cities. This is due our
+    % assumption that the plublic transport to cities with many connection (e.g. big station or airport)
+    % must be high.
+    % Introduction of tf or "city-size-factor" necessary? Or scaling with length of edge?
+    trsp_xy = (k(x)+k(y))/k_mean * tf; % parameters: tparam = transport parameter (e.g. a fixed percentage)
+    
+    % We take the population of the smaller cities to determine the total number of voyagers to avoid that
+    % a small city has more leaving voyagers than inhabitants.
+    % This is also based on our assumption that the number of voyagers in both direction of an edge is the same (ASSUMPTION 1).
+    % (ASSUMPTION 1 is important to keep the population in the cities constant).
+    if	N(x) < N(y)
+        tot_T(i) = round(trsp_xy * 1/k(x) * N(x));
+        
+    else % would be N(y) < N(x) or N(x) = N(y)
+        tot_T(i) = round(trsp_xy * 1/k(y) * N(y));
+        
+    end
+    
+end
+tot_T;
+end
